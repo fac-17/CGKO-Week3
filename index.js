14ee9e09fec41eb3e482d2409430a4b7a91ddce7
gminova <minova1995@gmail.com>
2019-07-17 23:47:22 +0100

    env testing

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const env = require("env2")("./env.json");
 
-console.log(process.env.API_KEY);
+console.log(process.env.DB_KEY);
+
+    function query() {
+
+    let search = document.getElementById("searchbutton");
+    search.addEventListener("click", query);
+    let xhr = new XMLHttpRequest();
+    let keyword = document.getElementById("searchfield").value;
+
+        let regex = keyword.trim();
+        keyword = regex.replace(/ /g, "+");
+
+
+    let apiKey = process.env.DB_KEY;
+    let url = `https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${DB_KEY}`;
+
+    xhr.onreadystatechange = function() {
+     if(xhr.readyState == 4 && xhr.status == 200) {
+         let response = JSON.parse(xhr.responseText);
+        console.log(response);
+         let result = document.createElement("span");
+         let form = document.querySelector(".search");
+         form.appendChild(result);
+         result.textContent = response;
+     }
+ };
+ xhr.open("GET", url, true);
+ xhr.send();
+}
