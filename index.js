634fc13dc7e6b1416744832c9d8244450efd6f66
frannyfra <francescadelaurentis1987@gmail.com>
2019-07-18 11:36:42 +0100

    working in progress

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-// let search = document.querySelector("#searchbutton");
-// search.addEventListener("click", query);
 
-// function query() {
-
-// let xhr = new XMLHttpRequest();
-//     let url = "https://data.police.uk/api/crimes-street/all-crime?poly=52.268,0.543:52.794,0.238:52.130,0.478";
-
-//     xhr.onreadystatechange = function() {
-//      if(xhr.readyState == 4 && xhr.status == 200) {
-//          let response = JSON.parse(xhr.responseText);
-//          let numCrimes = response.length;
-//          alert(numCrimes);
-//      }
-//  };
-//  xhr.open("GET", url, true);
-//  xhr.send();
-// }
 
 let search = document.querySelector("#searchbutton");
 search.addEventListener("click", query);
@@ -30,8 +13,30 @@ function query() {
       let response = JSON.parse(xhr.responseText);
       let lat = response.result.latitude;
       let long = response.result.longitude;
+      let loc = lat + long
     }
   };
   xhr.open("GET", urlLocation, true);
   xhr.send();
+  console.log(loc)
 }
+
+// let search = document.querySelector("#searchbutton");
+// search.addEventListener("click", query);
+
+let policeQuery = function() {
+
+let xhr = new XMLHttpRequest();
+    let url = "https://data.police.uk/api/crimes-at-location?date=2017-02&lat=52.629729&lng=-1.131592";
+
+    xhr.onreadystatechange = function() {
+     if(xhr.readyState == 4 && xhr.status == 200) {
+         let response = JSON.parse(xhr.responseText);
+         let numCrimes = response.length;
+         alert(numCrimes);
+     }
+ };
+ xhr.open("GET", url, true);
+ xhr.send();
+}
+
