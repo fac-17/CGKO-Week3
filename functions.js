ae6e3cd789135ac8e786b28a0447ad1088c9445b
Christine <christine.altmann@gmx.com>
2019-07-18 12:22:47 +0100

    added tests

--- functions.js
+++ functions.js
@@ -1,13 +1,20 @@
 const functions = {
-    test: function() {
-        return "Tape is working! Hooray!";
-    },
-    
-    trimSpace: function(str) {
-        return  str.trim();
-    }
-};
+  test: function() {
+    return "Tape is working! Hooray!";
+  },
 
+  trimSpace: function(str) {
+    return str.trim();
+  },
+
+  stringify: function(input) {
+    return input;
+  },
+
+  removeSpaces: function(postcode) {
+    return postcode.replace(/\s/g, "");
+  }
+};
 
 if (typeof module !== "undefined") {
   module.exports = functions;
--- tests.js
+++ tests.js
@@ -1,14 +1,28 @@
 const test = require("tape");
 const functions = require("./functions");
 
-test('Testing Tape is working with 1', function (t) {
-    t.equal(1, 1, 'One should equal one');
-    t.end();
-  });
+test("Testing Tape is working with 1", function(t) {
+  t.equal(1, 1, "One should equal one");
+  t.end();
+});
 
-  test("Testing regex trims empty sapces", function(t) {
-    const actual = functions.trimSpace("   NH67BG  ");
-    const expected = "NH67BG";
-    t.equal(actual, expected, "Should trim empty trailing spaces");
-    t.end();
-  })
+test("Testing regex trims empty sapces", function(t) {
+  const actual = functions.trimSpace("   NH67BG  ");
+  const expected = "NH67BG";
+  t.equal(actual, expected, "Should trim empty trailing spaces");
+  t.end();
+});
+
+test("Testing that the output is always a string", function(t) {
+  const actual = typeof functions.stringify("N98");
+  const expected = typeof "";
+  t.equal(actual, expected, " Should return string");
+  t.end();
+});
+
+test("Testing that the output has no empty spaces inside the string", function(t) {
+  const actual = functions.removeSpaces("N1 8EX");
+  const expected = "N18EX";
+  t.equal(actual, expected, "Should return without spaces");
+  t.end();
+});
