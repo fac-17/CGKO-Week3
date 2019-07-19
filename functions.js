2497375113b26d769937504be83060b5a04a17bf
gminova <minova1995@gmail.com>
2019-07-19 12:14:55 +0100

    tests updated, new API called to validate postcode relates #37 fixes #37'

--- functions.js
+++ functions.js
@@ -3,22 +3,8 @@ const functions = {
     return "Tape is working! Hooray!";
   },
 
-  trimSpace: function (str) {
-    return str.trim();
-  },
-
-  stringify: function (input) {
-    return input;
-  },
-
   removeSpaces: function (postcode) {
     return postcode.replace(/\s/g, "");
-  },
-
-  postcodeValidator: function (postcode) {
-    postcode = this.removeSpaces(postcode);
-    const pattern = /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})$/;
-    return pattern.test(postcode);
   },
 
   categoriesIterator: function (policeObj) {
